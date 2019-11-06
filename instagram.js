b603162678f67c75236e704774507eac2036a7b8
Ratchanon <cola.123xx@gmail.com>
2019-11-06 22:46:37 +0700

    full

--- instagram.js
+++ instagram.js
@@ -1,34 +1,43 @@
-$(function(){
-    // get instagram.json
-    $.get("https://www.instagram.com/explore/tags/thailand/?__a=1", function(data, status){
-        console.log(status);
-        console.log(data);
-        
-        
-        $("#location").text(data.graphql.hashtag.name);
-        //$("#slug").text(data.graphql.hashtag.slug);
-        $("#count").text(data.graphql.hashtag.edge_hashtag_to_media.count);
 
-        var imgUrl = data.graphql.hashtag.edge_hashtag_to_top_posts.edges[0].node.display_url;
-        // $("#post").attr("src", imgUrl); แบบไม่วนloop
 
-        for(node in data.graphql.hashtag.edge_hashtag_to_top_posts.edges){
-            var post = data.graphql.hashtag.edge_hashtag_to_top_posts.edges[node];
-            console.log(post.node.display_url);
-            var row =`<div class = "col-4">
-                            <img height = "300" width = "300" src="${post.node.display_url}" alt="">
-                            <div class = "row">
-                            <div class = "col">comments:${post.node.edge_media_to_comment.count}</div>
-                            <div class = "col">likes:${post.node.edge_liked_by.count}</div>
-                           
-                        </div>
-                        <div class="row">
-                        <div class = "col">caption:${post.node.edge_media_to_caption.edges[0].node.text}</div>
-                    </div>
-                </div>`;
-            $("#posts").append(row);
-        }
-
-    });
-
-});
+    $(function () {
+        var getSearch;
+    
+        $("#search").click(function () {
+            getSearch = $("#keyss").val();
+            $.get(`https://www.instagram.com/explore/tags/${getSearch}/?__a=1`, function (data, status) {
+                var location = data.graphql.hashtag.name;
+                var profile_pic_url = data.graphql.hashtag.profile_pic_url;
+                var post_count = data.graphql.hashtag.edge_hashtag_to_media.count;
+    
+                console.log(data);
+                $("#location").text("# " + location);
+                $("#profile").attr("src", profile_pic_url);
+                $("#post").text(post_count + " posts");
+    
+                var img_url = data.graphql.hashtag.edge_hashtag_to_media;
+    
+                for (node in img_url.edges) {
+                    var post = img_url.edges[node];
+                    var display_url = post.node.display_url;
+    
+                    var like_count = post.node.edge_liked_by.count
+                    var comment_count = post.node.edge_media_to_comment.count
+                    var comment = post.node.edge_media_to_caption.edges[0].node.text;
+                    var show_img = `
+                    <div class="col-4">
+                    <img src="${display_url}" 
+                    style = "height: 35vh;" class="img-thumbnail"><p></p>
+                    <div><i style='font-size:24px' class='far'>&#xf004;</i> ${like_count} &ensp;
+                    <i style='font-size:24px' class='far'>&#xf075;</i> ${comment_count}</div>
+                    <div class="card border-0 col text-left"data-spy="scroll" 
+                    style="position: relative; height: 4cm; overflow-y: scroll;">
+                    ${comment}</div><br>
+                    </div>`;
+    
+                    $("#showimg").append(show_img);
+                }
+    
+            });
+        });
+    })
